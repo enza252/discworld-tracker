314e60312260b1acd0d37a853889b73fd6858f5e
Ian Ensor <ian@ian-ensor.com>
2022-12-24 20:55:20 +0000

    wraps a button around the tiles

--- src/components/BookTiles/BookTiles.tsx
+++ src/components/BookTiles/BookTiles.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { books } from "../../data"
 import { Book } from "../../types";
-import { Card, CardContent, Grid, Paper, Typography } from "@mui/material";
+import { Card, CardContent, Grid, Button, Typography } from "@mui/material";
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles(() => ({
     root: {
-        minWidth: "100%",
-        minHeight: "100%"
+        width: "100%",
+        height: "100%"
     }
 }))
 
@@ -22,38 +22,38 @@ const BookTiles: React.FunctionComponent<BookTilesProps> = ({ filter }) => {
     const renderTiles = (books: Book[]) => {
         return books.map(book => (
             <Grid item xs={2} key={book.id}>
-                <Card className={classes.root}>
-                    <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Title:
-                        </Typography>
-                        <Typography>
-                            {book.title}
-                        </Typography>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Saga:
-                        </Typography>
-                        <Typography>
-                            {book.saga}
-                        </Typography>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Publication Order:
-                        </Typography>
-                        <Typography>
-                            {book.publicationOrder}
-                        </Typography>
-                    </CardContent>
-                </Card>
+                <Button className={classes.root}>
+                    <Card className={classes.root}>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Title:
+                            </Typography>
+                            <Typography sx={{ fontWeight: "bold" }}>
+                                {book.title}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Saga:
+                            </Typography>
+                            <Typography sx={{ fontWeight: "bold" }}>
+                                {book.saga}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Publication Order:
+                            </Typography>
+                            <Typography>
+                                {book.publicationOrder}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Button>
             </Grid>
         ))
     }
 
     return (
-        <Paper elevation={1}>
-            <Grid container direction="row" spacing={2} justifyContent="stretch" alignItems="stretch">
-                {filter ? renderTiles(books.filter(book => book.saga === filter)) : renderTiles(books)}
-            </Grid>
-        </Paper>
+        <Grid container direction="row" spacing={2} justifyContent="stretch" alignItems="stretch">
+            {filter ? renderTiles(books.filter(book => book.saga === filter)) : renderTiles(books)}
+        </Grid>
     )
 }
 
