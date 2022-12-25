e1a9786ebf9506d04a45f2541bca7a5305cc7e5d
Ian Ensor <ian@ian-ensor.com>
2022-12-25 11:49:56 +0000

    adds test coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -14,45 +14,42 @@ const sagas = [
   Saga.TIFFANY_ACHING_AND_NAC_MAC_FEEGLEs,
 ]
 
-test("tests expected books are in the doc", async () => {
-  render(<App />)
-
-  expect(screen.getByText("The Colour of Magic")).toBeInTheDocument()
-  expect(screen.getByText("The Light Fantastic")).toBeInTheDocument()
-  expect(screen.getByText("Mort")).toBeInTheDocument()
-  expect(screen.getByText("Sourcery")).toBeInTheDocument()
-  expect(screen.getByText("The Fifth Elephant")).toBeInTheDocument()
-  expect(screen.getByText("Going Postal")).toBeInTheDocument()
-  expect(screen.getByText("Wintersmith")).toBeInTheDocument()
-  expect(screen.getByText("Making Money")).toBeInTheDocument()
-  expect(screen.getByText("Raising Steam")).toBeInTheDocument()
-})
-
-test("tests publication orders are all present", () => {
-  render(<App />)
-
-  for (let i = 1; i <= 41; i++) {
-    expect(screen.getByText(i.toString())).toBeInTheDocument()
-  }
-})
-
-test("tests the sagas are listed in the side panel and the filtering works", async () => {
-  render(<App />)
-  sagas.forEach((s) => {
-    expect(screen.getByTestId(`${s}-filter`)).toBeInTheDocument()
+describe("tests the application", () => {
+  test("tests expected books are in the doc", async () => {
+    render(<App />)
+    expect(screen.getByText("The Colour of Magic")).toBeInTheDocument()
+    expect(screen.getByText("The Light Fantastic")).toBeInTheDocument()
+    expect(screen.getByText("Mort")).toBeInTheDocument()
+    expect(screen.getByText("Sourcery")).toBeInTheDocument()
+    expect(screen.getByText("The Fifth Elephant")).toBeInTheDocument()
+    expect(screen.getByText("Going Postal")).toBeInTheDocument()
+    expect(screen.getByText("Wintersmith")).toBeInTheDocument()
+    expect(screen.getByText("Making Money")).toBeInTheDocument()
+    expect(screen.getByText("Raising Steam")).toBeInTheDocument()
+    // tests publication orders 1 -> 41 are present
+    for (let i = 1; i <= 41; i++) {
+      expect(screen.getByText(i.toString())).toBeInTheDocument()
+    }
   })
-  await userEvent.click(screen.getByTestId("Death-filter"))
-  expect(screen.getByText("Mort")).toBeInTheDocument()
-  expect(screen.getByText("Reaper Man")).toBeInTheDocument()
-  expect(screen.getByText("Soul Music")).toBeInTheDocument()
-  expect(screen.getByText("Hogfather")).toBeInTheDocument()
-  expect(screen.getByText("Thief of Time")).toBeInTheDocument()
-  await userEvent.click(screen.getByTestId("Moist Von Lipwig-filter"))
-  expect(screen.getByText("Going Postal")).toBeInTheDocument()
-  expect(screen.getByText("Making Money")).toBeInTheDocument()
-  expect(screen.getByText("Raising Steam")).toBeInTheDocument()
 
-  expect(screen.getByTestId("view-by-publication-date")).toBeInTheDocument()
-  await userEvent.click(screen.getByTestId("view-by-publication-date"))
-  expect(screen.getAllByText("Saga:").length).toStrictEqual(41)
+  test("tests the sagas are listed in the side panel and the filtering works", async () => {
+    render(<App />)
+    sagas.forEach((s) => {
+      expect(screen.getByTestId(`${s}-filter`)).toBeInTheDocument()
+    })
+    await userEvent.click(screen.getByTestId("Death-filter"))
+    expect(screen.getByText("Mort")).toBeInTheDocument()
+    expect(screen.getByText("Reaper Man")).toBeInTheDocument()
+    expect(screen.getByText("Soul Music")).toBeInTheDocument()
+    expect(screen.getByText("Hogfather")).toBeInTheDocument()
+    expect(screen.getByText("Thief of Time")).toBeInTheDocument()
+    await userEvent.click(screen.getByTestId("Moist Von Lipwig-filter"))
+    expect(screen.getByText("Going Postal")).toBeInTheDocument()
+    expect(screen.getByText("Making Money")).toBeInTheDocument()
+    expect(screen.getByText("Raising Steam")).toBeInTheDocument()
+
+    expect(screen.getByTestId("view-by-publication-date")).toBeInTheDocument()
+    await userEvent.click(screen.getByTestId("view-by-publication-date"))
+    expect(screen.getAllByText("Saga:").length).toStrictEqual(41)
+  })
 })
